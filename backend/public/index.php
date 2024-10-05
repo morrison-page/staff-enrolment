3d3ecf84837ec843ad1012064ac9316724d4a0d5
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2024-10-05 20:01:00 +0100

    backend scaffold

---
+++ backend/public/index.php
@@ -0,0 +1,11 @@
+<?php
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    echo json_encode(['message' => 'Test']);
+}
+?>
---
+++ backend/tests/tests.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testAddition()
+    {
+        $this->assertEquals(4, 2 + 2);
+    }
+}
+
+?>
