6c5e14698ce8318035861b800ce9c90b73ef4d97
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2024-11-10 23:23:26 +0000

    Added updated routes

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,8 +2,12 @@
 require './headers.php';
 require '../classes/Router.php';
 require '../controllers/CoursesController.php';
+require '../controllers/UsersController.php';
+require '../controllers/EnrollmentsController.php';
 
 use Backend\Controllers\CoursesController;
+use Backend\Controllers\UsersController;
+use Backend\Controllers\EnrollmentsController;
 use Backend\Router;
 
 // Backend Entry Point
@@ -11,12 +15,26 @@ use Backend\Router;
 // Define Routes
 $router = new Router('/apis');
 
+// // Users
+// $router->addRoute('GET', '/users', [new UsersController(), 'index']);
+// $router->addRoute('GET', '/users/{id}', [new UsersController(), 'show']);
+// $router->addRoute('POST', '/users', [new UsersController(), 'create']);
+// $router->addRoute('PUT', '/users/{id}', [new UsersController(), 'update']);
+// $router->addRoute('DELETE', '/users/{id}', [new UsersController(), 'delete']);
+
 // Courses
 $router->addRoute('GET', '/courses', [new CoursesController(), 'index']);
 $router->addRoute('GET', '/courses/{id}', [new CoursesController(), 'show']);
 $router->addRoute('POST', '/courses', [new CoursesController(), 'create']);
 $router->addRoute('PUT', '/courses/{id}', [new CoursesController(), 'update']);
 $router->addRoute('DELETE', '/courses/{id}', [new CoursesController(), 'delete']);
+
+// // Enrollments
+// $router->addRoute('GET', '/enrollments', [new EnrollmentsController(), 'index']);
+// $router->addRoute('GET', '/enrollments/{id}', [new EnrollmentsController(), 'show']);
+// $router->addRoute('POST', '/enrollments', [new EnrollmentsController(), 'create']);
+// $router->addRoute('PUT', '/enrollments/{id}', [new EnrollmentsController(), 'update']);
+// $router->addRoute('DELETE', '/enrollments/{id}', [new EnrollmentsController(), 'delete']);
 
 $router->dispatch();
 
