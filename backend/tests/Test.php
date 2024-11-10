fc4d7d952a61fee27701c96ec03cf8d91df925e7
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2024-11-10 23:20:14 +0000

    Added Note for tests

--- backend/tests/Test.php
+++ backend/tests/Test.php
@@ -7,6 +7,16 @@ class Test extends TestCase
     public function testAddition()
     {
         $this->assertEquals(4, 2 + 2);
+
+        // MAKE SURE TO USE MAGIC CONSTANTS FOR TEST & DEBUG
+        // __DIR__ is the directory of the current file
+        // __FILE__ is the full path and filename of the file
+        // __LINE__ is the current line number of the file
+        // __CLASS__ is the class name
+        // __METHOD__ is the method name
+        // __FUNCTION__ is the function name
+        // __NAMESPACE__ is the name of the current namespace
+        // __TRAIT__ is the trait name 
     }
 }
 
