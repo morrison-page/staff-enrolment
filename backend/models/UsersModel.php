4d8c6fb7ee1dea52d9a111c7f6162f31d1c2f03d
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2024-11-11 23:31:58 +0000

    Added half complete password hashing

--- backend/models/UsersModel.php
+++ backend/models/UsersModel.php
@@ -17,6 +17,18 @@ class UsersModel {
 
     public static function create($data) {
         // Logic to create a new user
+        $salt = random_bytes(16);
+        $pepper = "superdupersecretpasswordpepper";
+        $password = "Hello World!";
+        $pepperedPassword = $password . $pepper;
+        $saltedPassword = bin2hex($salt) . $pepperedPassword;
+        $options = [
+            'memory_cost' => 1<<18, // 256 MB
+            'time_cost' => 12,      // 12 iterations
+            'threads' => 4          // 4 threads
+        ];
+        $hashedPassword = password_hash($saltedPassword, PASSWORD_ARGON2ID, $options);
+
     }
 
     public static function update($id, $data) {
