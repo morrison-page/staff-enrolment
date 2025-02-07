3cea5ed93dd3b6c1a377bd8f5ecf1d5a836c3782
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2025-02-07 22:42:22 +0000

    Added error suppression to htmlspecialchars in sanitisation method

--- backend/classes/Sanitisation.php
+++ backend/classes/Sanitisation.php
@@ -6,7 +6,7 @@ class Sanitisation {
     public static function sanitise($data) {
         $sanitisedData = [];
         foreach ($data as $key => $value) {
-            $sanitisedData[$key] = htmlspecialchars(trim($value), ENT_QUOTES, 'UTF-8');
+            $sanitisedData[$key] = @htmlspecialchars(trim($value), ENT_QUOTES, 'UTF-8');
         }
         return $sanitisedData;
     }
