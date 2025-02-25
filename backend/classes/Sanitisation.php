fa1c4829ae2fbdec09b950d8f0425fa3d5b773a9
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2025-02-25 19:30:42 +0000

    Added check for empty data in sanitisation method to prevent null value parsing errors

--- backend/classes/Sanitisation.php
+++ backend/classes/Sanitisation.php
@@ -2,8 +2,13 @@
 
 namespace Backend\Classes;
 
+use function PHPUnit\Framework\isEmpty;
+
 class Sanitisation {
     public static function sanitise($data) {
+        if (empty($data)){
+            return [];
+        }
         $sanitisedData = [];
         foreach ($data as $key => $value) {
             $sanitisedData[$key] = @htmlspecialchars(trim($value), ENT_QUOTES, 'UTF-8');
