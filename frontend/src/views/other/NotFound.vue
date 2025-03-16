13f81573e90d74915b625507e9b7b6ec92fdf032
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2025-03-16 22:13:23 +0000

    Added invalid route page with redirect

---
+++ frontend/src/views/other/NotFound.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="h-100">
+    <div class="container h-100">
+      <div class="row justify-content-sm-center h-100">
+        <div class="col-xxl-5 col-xl-5 col-lg-5 col-md-5 col-sm-9" style="padding-top: 23vh">
+          <div class="card shadow-lg">
+            <div class="card-body p-5">
+              <h1>404 - Page Not Found</h1>
+              <p>The page you are looking for does not exist.</p>
+              <router-link to="/">Go to Home</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
