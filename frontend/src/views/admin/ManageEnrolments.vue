dc46dafb565ab94a564fb006b0c4ac72f78a3254
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2025-03-17 15:14:21 +0000

    Refactored manage enrolments view to use TypeScript with axios config and split enrolments into historic and future

--- frontend/src/views/admin/ManageEnrolments.vue
+++ frontend/src/views/admin/ManageEnrolments.vue
@@ -1,9 +1,9 @@
-<script setup>
+<script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
 import { ref, onMounted } from 'vue';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
-import axios from 'axios';
+import _axios from '@/config/axios';
 
 
 // Admin Access Level Check
@@ -14,15 +14,23 @@ if (access_level !== 'admin') {
   router.push({name: 'NotFound'});
 }
 
-const enrolments = ref([]);
-const groupedEnrolments = ref({});
+interface Enrolment {
+  enrolment_id: string;
+  user_id: string;
+  user: string;
+  email: string;
+  course_id: string;
+  course_title: string;
+  course_date: string;
+}
+
+const enrolments = ref<Enrolment[]>([]);
+const groupedEnrolments = ref<Record<string, Enrolment[]>>({});
 
 // TODO: Refactor to use vuex
 const fetchEnrolments = async () => {
   try {
-    const response = await axios.get(`${import.meta.env.VITE_BACKEND_API_URL}/enrolments`, {
-      withCredentials: true,
-    });
+    const response = await _axios.get('/enrolments');
     enrolments.value = response.data;
     groupEnrolmentsByCourse();
   } catch (error) {
@@ -30,11 +38,9 @@ const fetchEnrolments = async () => {
   }
 };
 // TODO: Refactor to use vuex
-const deleteEnrolment = async (enrolmentId) => {
+const deleteEnrolment = async (enrolmentId: string) => {
   try {
-    await axios.delete(`${import.meta.env.VITE_BACKEND_API_URL}/enrolments/${enrolmentId}`, {
-      withCredentials: true,
-    });
+    await _axios.delete(`/enrolments/${enrolmentId}`);
     await fetchEnrolments();
   } catch (error) {
     console.error('Error deleting enrolment:', error);
@@ -42,7 +48,7 @@ const deleteEnrolment = async (enrolmentId) => {
 };
 
 const groupEnrolmentsByCourse = () => {
-  groupedEnrolments.value = enrolments.value.reduce((acc, enrolment) => {
+  groupedEnrolments.value = enrolments.value.reduce((acc: Record<string, Enrolment[]>, enrolment) => {
     if (!acc[enrolment.course_id]) {
       acc[enrolment.course_id] = [];
     }
@@ -51,6 +57,14 @@ const groupEnrolmentsByCourse = () => {
   }, {});
 };
 
+function isPastDate(date: string): boolean {
+  const [day, month, year] = date.split('/').map(Number);
+  const inputDate = new Date(year, month - 1, day);
+  const today = new Date();
+  today.setHours(0, 0, 0, 0); // Set to midnight to compare only dates
+  return inputDate < today;
+}
+
 onMounted(() => {
   fetchEnrolments();
 });
@@ -58,34 +72,68 @@ onMounted(() => {
 
 <template>
   <AppLayout>
-    <div class="container mt-4">
-      <h1>Manage Enrolments</h1>
-      <br>
+    <div class="container mt-3">
+      <h2>Manage Future Enrolments</h2>
+      <hr><br>
       <div v-for="(enrolments, courseId) in groupedEnrolments" :key="courseId">
-        <h4>{{ enrolments[0].course_title }} - {{ enrolments[0].course_date }}</h4>
-        <hr>
-        <table class="table table-striped shadow-lg">
-          <caption>{{ enrolments[0].course_id }}</caption>
-          <thead>
-            <tr>
-              <th scope="col">Enrolment ID</th>
-              <th scope="col">User</th>
-              <th scope="col">Email</th>
-              <th scope="col">Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="enrolment in enrolments" :key="enrolment.user_id">
-              <td>{{ enrolment.enrolment_id }}</td>
-              <td>{{ enrolment.user }}</td>
-              <td>{{ enrolment.email }}</td>
-              <td>
-                <button class="btn btn-danger btn-sm" @click="deleteEnrolment(enrolment.enrolment_id)">Delete</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <br>
+        <div v-if="!isPastDate(enrolments[0].course_date)">
+          <h4>{{ enrolments[0].course_title }} - {{ enrolments[0].course_date }}</h4>
+          <hr>
+          <table class="table table-striped shadow-lg">
+            <caption>{{ enrolments[0].course_id }}</caption>
+            <thead>
+              <tr>
+                <th scope="col">Enrolment ID</th>
+                <th scope="col">User</th>
+                <th scope="col">Email</th>
+                <th scope="col">Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="enrolment in enrolments" :key="enrolment.user_id">
+                <td>{{ enrolment.enrolment_id }}</td>
+                <td>{{ enrolment.user }}</td>
+                <td>{{ enrolment.email }}</td>
+                <td>
+                  <button class="btn btn-danger btn-sm" @click="deleteEnrolment(enrolment.enrolment_id)">Delete</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <br>
+        </div>
+      </div>
+    </div>
+    <div class="container mt-3">
+      <h2>Manage Historic Enrolments</h2>
+      <hr><br>
+      <div v-for="(enrolments, courseId) in groupedEnrolments" :key="courseId">
+        <div v-if="isPastDate(enrolments[0].course_date)">
+          <h4>{{ enrolments[0].course_title }} - {{ enrolments[0].course_date }}</h4>
+          <hr>
+          <table class="table table-striped shadow-lg">
+            <caption>{{ enrolments[0].course_id }}</caption>
+            <thead>
+              <tr>
+                <th scope="col">Enrolment ID</th>
+                <th scope="col">User</th>
+                <th scope="col">Email</th>
+                <th scope="col">Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="enrolment in enrolments" :key="enrolment.user_id">
+                <td>{{ enrolment.enrolment_id }}</td>
+                <td>{{ enrolment.user }}</td>
+                <td>{{ enrolment.email }}</td>
+                <td>
+                  <button class="btn btn-danger btn-sm" @click="deleteEnrolment(enrolment.enrolment_id)">Delete</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <br>
+        </div>
       </div>
     </div>
   </AppLayout>
