780deeabf050fa95f67e3a835d9b6525c423c28d
Morrison Page <104316350+morrison-page@users.noreply.github.com>
2025-03-17 17:29:50 +0000

    Added form data validation to prevent submission with invalid content

--- frontend/src/views/admin/forms/CourseForm.vue
+++ frontend/src/views/admin/forms/CourseForm.vue
@@ -15,10 +15,12 @@ const course = ref({
   course_id: '',
   course_title: '',
   course_date: '',
-  course_duration: 0,
-  max_attendees: 0,
+  course_duration: 1,
+  max_attendees: 1,
   description: '',
 });
+
+const formRef = ref(null);
 
 const dateToHTMLdate = (dateString) => {
   if (!dateString) return ''; // Dodgy Type Fix
@@ -54,6 +56,11 @@ const fetchCourse = async () => {
 const handleCancel = () => { router.push('/manage/courses'); };
 
 const handleSubmit = async () => {
+  if (formRef.value.checkValidity() === false) {
+    formRef.value.classList.add('was-validated');
+    return;
+  }
+
   if (isEditMode.value) {
     course.value.course_date = htmlDateToDate(course.value.course_date);
     await store.dispatch('courses/updateCourse', course.value);
@@ -86,34 +93,39 @@ watch(route, async (newRoute) => {
     <div class="card p-4 mt-5">
       <h1>{{ isEditMode ? 'Edit Course' : 'Create Course' }}</h1>
       <hr>
-      <form @submit.prevent="handleSubmit">
+      <form ref="formRef" @submit.prevent="handleSubmit" novalidate>
         <div class="mb-3">
           <label for="course_id" class="form-label">Course ID</label>
           <input :value="course.course_id" id="course_title" type="text" class="form-control" disabled/>
         </div>
         <div class="mb-3">
           <label for="course_title" class="form-label">Course Title</label>
-          <input v-model="course.course_title" id="course_title" type="text" class="form-control" required/>
+          <input v-model="course.course_title" id="course_title" type="text" class="form-control" required minlength="5" maxlength="255"/>
+          <div class="invalid-feedback">Please provide a course title (5-255 characters).</div>
         </div>
         <div class="mb-3">
           <label for="course_date" class="form-label">Course Date</label>
-          <input v-model="course.course_date" id="course_date" type="date" class="form-control" required/>
+          <input v-model="course.course_date" id="course_date" type="date" class="form-control" required minlength="10" maxlength="10"/>
+          <div class="invalid-feedback">Please provide a valid course date (DD/MM/YYYY)</div>
         </div>
         <div class="mb-3">
           <label for="course_duration" class="form-label">Course Duration (days)</label>
-          <input v-model="course.course_duration" id="course_duration" type="number" class="form-control" required/>
+          <input v-model="course.course_duration" id="course_duration" type="number" class="form-control" required min="1" max="365"/>
+          <div class="invalid-feedback">Please provide a course duration (1-365 days).</div>
         </div>
         <div class="mb-3">
           <label for="max_attendees" class="form-label">Max Attendees</label>
-          <input v-model="course.max_attendees" id="max_attendees" type="number" class="form-control" required/>
+          <input v-model="course.max_attendees" id="max_attendees" type="number" class="form-control" required min="1" max="10000"/>
+          <div class="invalid-feedback">Please provide the maximum number of attendees (1-10,000).</div>
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
-            <textarea v-model="course.description" style="max-height: 200px;" id="description" class="form-control" required></textarea>
+            <textarea v-model="course.description" style="max-height: 200px;" id="description" class="form-control" required minlength="2" maxlength="100"></textarea>
+            <div class="invalid-feedback">Please provide a description (2-100 characters).</div>
         </div>
         <div class="d-flex gap-2">
           <button class="btn btn-secondary w-100" type="button" @click="handleCancel">Cancel</button>
-          <button class="btn btn-primary w-100" @click="" type="submit">{{ isEditMode ? 'Update' : 'Create' }}</button>
+          <button class="btn btn-primary w-100" type="submit">{{ isEditMode ? 'Update' : 'Create' }}</button>
         </div>
       </form>
     </div>
